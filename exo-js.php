4b4d54ac1ed288ecb439c02428dc07cd877db461
StevenFA31 <stevenferreiraalves@gmail.com>
2022-11-18 16:08:47 +0100

    Les 2 exercices dans le README terminé

---
+++ exo-js.php
@@ -0,0 +1,52 @@
+<?php
+
+// EXERCICE 1 // 
+
+// $henry = readline("Quel est la couleur du cheval blanc d'Henri IV ?");
+// $nain = readline("Combien y a-t-il de 7 nains ?");
+
+// EXERCICE 2 //
+
+// function result()
+// {
+//     $operator = ["+", "-", "*"];
+//     $op = $operator[rand(0,2)];
+
+//     $a = rand(0, 9);
+//     $b = rand(0, 9);
+//     $c = 0;
+
+//     switch ($op) {
+//         case "+":
+//             $insert = readline("Combien font ". $a ." + ". $b ." ? ");
+//             $c = $a + $b;
+//             if ($c == $insert){
+//                 echo("Bien vu le bougre\n");
+//             } else {
+//                 echo("Perdu, le résultat était " . $c);
+//             }
+//             break;
+//         case "-":
+//             $insert = readline("Combien font ". $a ." - ". $b ." ? ");
+//             $c = $a - $b;
+//             if ($c == $insert){
+//                 echo("Bien vu le bougre\n");
+//             } else {
+//                 echo("Perdu, le résultat était " . $c);
+//             }
+//             break;
+//         case "*":
+//             $insert = readline("Combien font ". $a ." * ". $b ." ? ");
+//             $c = $a * $b;
+//             if ($c == $insert){
+//                 echo("Bien vu le bougre\n");
+//             } else {
+//                 echo("Perdu, le résultat était " . $c);
+//             }
+//             break;
+//     }
+
+// }
+// result();
+
+?>
